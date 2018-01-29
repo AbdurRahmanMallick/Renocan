a9cacc2b8aea4fff20179e97e9fcc7c7e6b239ed
Abdur Rahman <ABDURRAHMAN@DESKTOP-JBKHP58>
2018-01-29 16:36:36 +0500

    Version1

---
+++ Renocan/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Renocan.Models;
+using Renocan.Dtos;
+
+namespace Renocan.App_Start
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Client_Signup, Client_SignupDto>();
+            Mapper.CreateMap<Client_SignupDto, Client_Signup>();
+        }
+    }
+}

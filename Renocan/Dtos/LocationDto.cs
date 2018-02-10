2336e8e143212b7fcdc4f4455cdb2e278c0ac517
Zalmi <Zalmi@PATOKI-PC>
2018-02-10 14:05:41 +0500

    Location

---
+++ Renocan/Dtos/LocationDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Renocan.Dtos
+{
+    public class LocationDto
+    {
+        public int Location_ID { get; set; }
+
+        [Required]
+        public string Location_Name { get; set; }
+
+        [Required]
+        public string Postal_Code { get; set; }
+
+    }
+}
